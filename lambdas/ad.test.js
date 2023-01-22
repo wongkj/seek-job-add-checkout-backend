0c82ab785e534efee60479fd529d38791d1d9c13
JasonWong <wongkj.11@gmail.com>
2023-01-22 13:46:10 +1100

    test: write failed test

--- lambdas/ad.test.js
+++ lambdas/ad.test.js
@@ -10,6 +10,18 @@ describe('createAd', () => {
     })
   })
   describe('failed to create a new ad in the adTable', () => {
+    const event = {
+      resource: '/',
+      path: '/ad',
+      httpMethod: 'POST',
+      body: {
+          name: 'New Ad Type',
+          price: 400.00,
+          description: 'This is a new ad type.'
+      },
+      queryStringParameters: null,
+      pathParameters: null
+    }
 
     beforeEach(() => {
 
@@ -18,6 +30,17 @@ describe('createAd', () => {
 
     })
     test('incorrect HTTP Method returns a 405 Method Not Allowed', () => {
+      const eventNotPostMethod = {
+        ...event,
+        httpMethod: 'GET'
+      }
+      const expectedResponse = {
+        statusCode: 405,
+        statusType: 'Method Not Allowed',
+        body: JSON.stringify({ message: 'Must use POST Method to add a user.' })
+      }
+      const response = createAd(eventNotPostMethod)
+      expect(response).toMatchObject(expectedResponse)
 
     })
     test('put action in the Dynamo adTable causing an error then 500 Internal Server Error is returned', () => {
