de87ac095330e78733e69033255d82201a482576
JasonWong <wongkj.11@gmail.com>
2023-01-22 14:14:27 +1100

    code: wrote implementation of the checkAdObject in our handler. the tests now pass

--- lambdas/ad.js
+++ lambdas/ad.js
@@ -1,7 +1,14 @@
 const Responses = require('../common/Responses');
+const { checkAdObject } = require('../common/utils');
 
 module.exports.createAd = async event => {
   if (event.httpMethod !== 'POST') {
     return Responses._405({ message: 'Must use POST Method to add a user.' })
-  }        
+  }
+  const ad = typeof event.body === 'string' ? JSON.parse(event.body) : event.body
+  const validAdObject = checkAdObject(ad)
+  if (!validAdObject) {
+    return Responses._400({ message: 'Ad properties were incorrect. Cannot add user to database.' })
+  }
+
 }
